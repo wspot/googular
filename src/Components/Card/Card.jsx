f8726297f55f0489b1bf248b759e9af4fa746bb3
wspot <iwspot1@gmail.com>
2020-06-08 22:26:47 +0100

    card component

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React  from 'react';
+
+function Card({data}) {
+
+    return (
+        <div className="col-sm-4 mt-2">
+            <div className="card shadow rounded-0">
+                <div className="card-body">
+                    <button type="button" className="close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h5 className="card-title">{data.title} </h5>
+                    <p className="card-text">{data.description}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
