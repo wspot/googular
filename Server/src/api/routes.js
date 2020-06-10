d1694eb9a956eb9a301406a57b8664dc60d79fb4
wspot <iwspot1@gmail.com>
2020-06-10 23:56:51 +0100

    Server : implementing  filtered search api route

--- Server/src/api/routes.js
+++ Server/src/api/routes.js
@@ -11,6 +11,18 @@ router.get('/searchResults', async (req, res, next) => {
     }
 });
 
+router.post('/getfilteredSearch', async (req, res, next) => {
+    try {
+        let topics = ('topics' in req.body) ? req.body.topics : [];
+        let query = topics.length > 0 ? { 'topics': { $in: topics } } : {};
+        let result = await searchResult.find(query);
+        res.json({ result });
+
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.post('/searchResults', async (req, res, next) => {
     try {
         const entity = new searchResult(req.body);
